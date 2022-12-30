c7c01aca344867641e507e42845758f8bcc583c7
22mh1a05j1 <119724202+22mh1a05j1@users.noreply.github.com>
2022-12-31 00:47:24 +0530

    #include<stdio.h> int main() {     int x;     scanf("%d",&x);     if(x%5!=0 &&x%10!=0)     {         printf("%d",-1);     }     else     {         printf("%d",x/10+(x%10)/5);     } }

---
+++ Kohli_and_Coins.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(x%5!=0 &&x%10!=0)
+    {
+        printf("%d",-1);
+    }
+    else
+    {
+        printf("%d",x/10+(x%10)/5);
+    }
+}
