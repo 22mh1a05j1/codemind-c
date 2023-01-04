ac822d71c70acc9affcafebc574dd7d6583a2039
22mh1a05j1 <119724202+22mh1a05j1@users.noreply.github.com>
2023-01-04 21:26:21 +0530

    #include<stdio.h> int main()
        {
        int n,k,x,y;
        scanf("%d%d%d%d",&n,&k,&x,&y);
        int a=k*x+(n-k)*y;
        if(x<y)
        {
            printf("%d",n*x);
        }
        else
        {
            printf("%d",a);
        }
    }

---
+++ Lamps.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+
+    {
+    int n,k,x,y;
+    scanf("%d%d%d%d",&n,&k,&x,&y);
+    int a=k*x+(n-k)*y;
+    if(x<y)
+    {
+        printf("%d",n*x);
+    }
+    else
+    {
+        printf("%d",a);
+    }
+}
