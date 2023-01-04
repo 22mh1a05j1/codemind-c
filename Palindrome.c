d62ea00a4f3192979ef2b4c9bc26a693ed8222a7
22mh1a05j1 <119724202+22mh1a05j1@users.noreply.github.com>
2023-01-04 23:22:41 +0530

    #include<stdio.h> int main() {     int n,r,a,rv=0;     scanf("%d",&n);     int t=n;     while(n!=0)     {         r=n%10;         rv=rv*10+r;         n=n/10;     }     if(rv==t)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Palindrome.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,a,rv=0;
+    scanf("%d",&n);
+    int t=n;
+    while(n!=0)
+    {
+        r=n%10;
+        rv=rv*10+r;
+        n=n/10;
+    }
+    if(rv==t)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
