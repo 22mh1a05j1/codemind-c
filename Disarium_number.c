5ff17fe49a51f797ae4f4898838c2a7e460d6a1e
22mh1a05j1 <119724202+22mh1a05j1@users.noreply.github.com>
2023-01-18 21:45:11 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,o=0,sum=0,rem=0,r=0,c;     scanf("%d",&n);     int t=n;     int temp=n;     while(n!=0)     {         r=n%10;         c++;         n=n/10;     }     while(t!=0)     {         rem=t%10;         sum=sum+pow(rem,c);         c=c-1;         t=t/10;     }     if(sum==temp)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Disarium_number.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,o=0,sum=0,rem=0,r=0,c;
+    scanf("%d",&n);
+    int t=n;
+    int temp=n;
+    while(n!=0)
+    {
+        r=n%10;
+        c++;
+        n=n/10;
+    }
+    while(t!=0)
+    {
+        rem=t%10;
+        sum=sum+pow(rem,c);
+        c=c-1;
+        t=t/10;
+    }
+    if(sum==temp)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
