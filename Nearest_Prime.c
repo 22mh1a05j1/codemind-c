33cc2e27d32884c86e6c28702019aa1ca6302b07
22mh1a05j1 <119724202+22mh1a05j1@users.noreply.github.com>
2023-05-23 22:48:46 +0530

    #include<stdio.h> int prime(int n) {     int c=0;     for(int i=1;i<=n;i++)     {         if(n%i==0)         {             c++;         }     }     if(c==2)     {         return 1;     }     else     {         return 0;     } } int main() {     int x,f,b;     scanf("%d",&x);     for(int i=1;i<=x;i++)     {         int n;         scanf("%d",&n);         for(int i=n;;i++)         {             if(prime(i))             {                 f=i;                 break;             }         }         for(int i=n;;i--)         {             if(prime(i))             {                 b=i;                 break;             }         }         int d1=f-n;         int d2=n-b;         if(d1<d2)         {             printf("%d ",f);         }         else if(d2<=d1)         {             printf("%d ",b);         }     }
    }

--- Nearest_Prime.c
+++ Nearest_Prime.c
@@ -1,43 +1,4 @@
 #include<stdio.h>
-int prime(int);
-int main()
-{
-    int n,i;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        int x,f,b,d1,d2;
-        scanf("%d",&x);
-        for(int i=x;;i++)
-        {
-            if(prime(i))
-            {
-                f=i;
-                break;
-            }
-        }
-        for(int i=x;;i--)
-        {
-            if(prime(i))
-            {
-                b=i;
-                break;
-            }
-        }
-        d1=f-x;
-        d2=x-b;
-        if(d1>=d2)
-        {
-            printf("%d
-",b);
-        }
-        else if(d2>d1)
-        {
-            printf("%d
-",f);
-    }
-    }
-}
 int prime(int n)
 {
     int c=0;
@@ -55,7 +16,44 @@ int prime(int n)
     else
     {
         return 0;
-
-        
     }
+}
+int main()
+{
+    int x,f,b;
+    scanf("%d",&x);
+    for(int i=1;i<=x;i++)
+    {
+        int n;
+        scanf("%d",&n);
+        for(int i=n;;i++)
+        {
+            if(prime(i))
+            {
+                f=i;
+                break;
+            }
+        }
+        for(int i=n;;i--)
+        {
+            if(prime(i))
+            {
+                b=i;
+                break;
+            }
+        }
+        int d1=f-n;
+        int d2=n-b;
+        if(d1<d2)
+        {
+            printf("%d
+",f);
+        }
+        else if(d2<=d1)
+        {
+            printf("%d
+",b);
+        }
+    }
+    
 }
