8c69925773783db463eaa249f72307597fcbec1c
22mh1a05j1 <119724202+22mh1a05j1@users.noreply.github.com>
2023-05-23 23:30:33 +0530

    #include<stdio.h> int main() {     int n,c=0;     scanf("%d",&n);     for(int i=1;i<=n;i++)     {         int a;         scanf("%d",&a);         if(n<=a)         {             c++;         }     }     if(c==0)     {         printf("YES");     }     else     {         printf("NO");     } }

--- Can_Robber_Escape?.c
+++ Can_Robber_Escape?.c
@@ -3,14 +3,11 @@ int main()
 {
     int n,c=0;
     scanf("%d",&n);
-    int a[n];
-    for(int i=0;i<n;i++)
+    for(int i=1;i<=n;i++)
     {
-        scanf("%d",&a[i]);
-    }
-    for(int i=0;i<n;i++)
-    {
-        if(n<=a[i])
+        int a;
+        scanf("%d",&a);
+        if(n<=a)
         {
             c++;
         }
