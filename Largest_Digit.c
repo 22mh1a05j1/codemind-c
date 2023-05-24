e9ed4738da3378dc1df42307588f994ebeba39f1
22mh1a05j1 <119724202+22mh1a05j1@users.noreply.github.com>
2023-05-24 15:11:02 +0530

    #include<stdio.h> int main() {     int n,max=0,c=0;     scanf("%d",&n);     int i=0;     int a[100];     while(n!=0)     {         int r=n%10;         a[i]=r;         i++;         c++;         n=n/10;     }     for(int i=0;i<c;i++)     {         if(a[i]>max)         {             max=a[i];         }     }     printf("%d",max); }

--- Largest_Digit.c
+++ Largest_Digit.c
@@ -1,16 +1,25 @@
 #include<stdio.h>
 int main()
 {
-    int n,max=0,rem;
+    int n,max=0,c=0;
     scanf("%d",&n);
+    int i=0;
+    int a[100];
     while(n!=0)
     {
-        rem=n%10;
-        if(rem>max)
-        {
-            max=rem;
-        }
+        int r=n%10;
+        a[i]=r;
+        i++;
+        c++;
         n=n/10;
+    }
+    for(int i=0;i<c;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
     }
     printf("%d",max);
 }
+
