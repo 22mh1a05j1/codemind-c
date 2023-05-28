013540eb5a0428422351a24014c1f0d57b001107
22mh1a05j1 <119724202+22mh1a05j1@users.noreply.github.com>
2023-05-28 15:53:29 +0530

    #include<stdio.h> int main() {     int r,c,max1=0,max2=0;     scanf("%d%d",&r,&c);     int mat[r][c];     for(int i=0;i<r;i++)     {         for(int j=0;j<c;j++)         {             scanf("%d",&mat[i][j]);         }     }     for(int i=0;i<r;i++)     {         int s=0;         for(int j=0;j<c;j++)         {             s=s+mat[i][j];         }         if(s>max1)         {             max1=s;         }     }     for(int i=0;i<c;i++)     {         int s=0;         for(int j=0;j<r;j++)         {             s=s+mat[j][i];         }         if(s>max2)         {             max2=s;         }     }     if(max1>max2)     {         printf("%d",max1);     }     else     {         printf("%d",max2);     } }

--- Maximum_sum_of_row_wise_sum_or_column_wise_sum_of_matrix.c
+++ Maximum_sum_of_row_wise_sum_or_column_wise_sum_of_matrix.c
@@ -1,46 +1,46 @@
 #include<stdio.h>
 int main()
 {
-    int r,c,max1=0,max=0;
+    int r,c,max1=0,max2=0;
     scanf("%d%d",&r,&c);
-    int a[r][c];
-    for(int i=1;i<=r;i++)
+    int mat[r][c];
+    for(int i=0;i<r;i++)
     {
-        for(int j=1;j<=c;j++)
+        for(int j=0;j<c;j++)
         {
-            scanf("%d",&a[i][j]);
+            scanf("%d",&mat[i][j]);
         }
     }
-   for(int i=1;i<=r;i++)
-   {
-       int s=0;
-       for(int j=1;j<=c;j++)
-       {
-           s=s+a[i][j];
-       }
-       if(max<s)
-       {
-           max=s;
-       }
-   }
-   for(int i=1;i<=c;i++)
-   {
-       int sum=0;
-       for(int j=1;j<=r;j++)
-       {
-           sum=sum+a[j][i];
-       }
-       if(sum>max1)
-       {
-           max1=sum;
-       }
-   }
-   if(max>max1)
-   {
-       printf("%d",max);
-   }
-   else
-   {
-       printf("%d",max1);
-   }
-}   
+    for(int i=0;i<r;i++)
+    {
+        int s=0;
+        for(int j=0;j<c;j++)
+        {
+            s=s+mat[i][j];
+        }
+        if(s>max1)
+        {
+            max1=s;
+        }
+    }
+    for(int i=0;i<c;i++)
+    {
+        int s=0;
+        for(int j=0;j<r;j++)
+        {
+            s=s+mat[j][i];
+        }
+        if(s>max2)
+        {
+            max2=s;
+        }
+    }
+    if(max1>max2)
+    {
+        printf("%d",max1);
+    }
+    else
+    {
+        printf("%d",max2);
+    }
+}
